333de78831c372c15a2149675be46a87bafb969b
JavierFranciscopd <javierfranciscopd1@gmail.com>
2022-08-29 14:53:02 +0200

    subida de archivos

---
+++ src/test/java/com/example/systemerp/SystemErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
