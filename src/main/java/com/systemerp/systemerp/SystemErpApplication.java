876c77b4eb9deebe2d12fc4cd7e092700bd71713
JavierFranciscopd <javierfranciscopd1@gmail.com>
2022-08-29 18:31:47 +0200

    Creacion de clases Empresa, Empleado y Transaccion con conexion de muchos a uno y uno a muchos

---
+++ src/main/java/com/systemerp/systemerp/SystemErpApplication.java
@@ -0,0 +1,13 @@
+package com.systemerp.systemerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systemerp/systemerp/SystemErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemerp.systemerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
